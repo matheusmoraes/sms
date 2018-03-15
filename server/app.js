fc8510f0b1c714162ff923bf0c69ed9bc662ef32
Matheus Negrão <matheus.moraes@moneto.com.br>
2018-03-15 19:48:46 -0300

    Added Winston for logging :loud-sound:

--- server/app.js
+++ server/app.js
@@ -2,11 +2,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var mongoose = require('mongoose');
+
+var winston = require('winston');
+var expressWinston = require('express-winston');
+require('winston-mongodb');
+
 var config = require('./config/config');
 var db = require('./config/connect')(config.db);
 
 var app = express();
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.MongoDB({
+      db: config.db
+    }),
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ],
+  meta: true,
+  msg: "HTTP {{req.method}} {{req.url}}",
+  expressFormat: true,
+  colorize: false,
+}));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
