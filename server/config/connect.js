90cc6488a34daa53bb62e4bdcedb5d6bfd126d50
Matheus Negrão <matheus.moraes@moneto.com.br>
2018-03-15 18:38:06 -0300

    Improved db connection logs

--- server/config/connect.js
+++ server/config/connect.js
@@ -8,29 +8,18 @@ const db = (mongo_url) => {
   if (process.env.NODE_ENV === 'test') {
     mockgoose.prepareStorage().then(() => {
       mongoose.connect(mongo_url, (err) => {
-        console.log('conectou os caraio');
+        console.log('Connected to TEST database');
       });
     });
 
     let mongo = mongoose.connection;
-    mongo.once('open', () => {
-      console.log('Connected to TEST DATABASE');
-    })
-    mongo.once('error', (e) => {
-      console.log('Deu merda', e);
-    })
-
     return mongo;
   } else {
     mongoose.connect(mongo_url);
     let mongo = mongoose.connection;
     mongo.once('open', () => {
-      console.log('Connected to PROD DATABASE');
+      console.log('Connected to PROD database');
     })
-    // mongo.connection.on('connected', function() {
-    //   console.log('Mongoose default connection ', config.db);
-    // });
-
     return mongo;
   }
 }
