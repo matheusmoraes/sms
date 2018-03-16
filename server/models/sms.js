3ac343fcf6dc61426affed09c1825f2c7c347c14
Matheus Negrão <matheus.moraes@moneto.com.br>
2018-03-16 11:24:37 -0300

    Added TextToSMSParser

--- server/models/sms.js
+++ server/models/sms.js
@@ -2,10 +2,39 @@
 class TextToSMSParser {
   constructor() {
     this.letters = {
+      'abc': 2,
+      'def': 3,
+      'ghi': 4,
+      'jkl': 5,
+      'mno': 6,
+      'pqrs': 7,
+      'tuv': 8,
+      'wxyz': 9,
+      ' ': 0
     }
   }
 
   parse(text) {
+    let result = [];
+    let lastChar = undefined;
+    text = text.toLowerCase();
+    for (let char of text) {
+      for(let key in this.letters) {
+        if (key.indexOf(char) != -1) {
+          let count = key.indexOf(char) + 1;
+          if (this.letters[key] == lastChar) {
+            result.push('_');
+          }
+          for(let i=0; i < count; i++) {
+            result.push(this.letters[key]);
+          }
+          lastChar = this.letters[key];
+          break;
+        }
+      }
+    }
+
+    return result.join('');
   }
 }
 
--- server/test/models/sms.spec.js
+++ server/test/models/sms.spec.js
@@ -4,17 +4,23 @@ const TextToSMSParser = require('../../models/sms');
 const expect = require('chai').expect;
 
 
-describe('TextToSMSParser module', () => {
-  it('should export a function', () => {
-    expect(TextToSMSParser).to.be.a('function');
-  });
+describe('TextToSMSParser', () => {
+
+  let parser = new TextToSMSParser();
 
   describe('module', () => {
-    let parser;
-
-    beforeEach(() => {
-
+    it('should export a function', () => {
+      expect(TextToSMSParser).to.be.a('function');
     });
-
   });
+
+  describe('.parse', () => {
+    it('should parse the message', () => {
+      let message = 'TESTE DE MESA';
+      let parsedMessage = parser.parse(message);
+      expect(parsedMessage).to.be.equal('833777783303_33063377772');
+    });
+  });
+
+
 });
